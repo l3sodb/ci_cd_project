f289374b1c137ea54b8ff8c35b7df9161d3ecd3b
Pacifique IKIREZI <122596075+l3sodb@users.noreply.github.com>
2024-11-15 15:14:02 +0200

    server updated

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/',(req,res)=>{
     res.send('hello world');
 
 })
+app.get('/users',(req,res)=>{
+    res.send('This is users root');
+})
 app.listen(3000, ()=>{
     console.log('server is running on port 3000')
 })
